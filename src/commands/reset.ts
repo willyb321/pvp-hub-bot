729c34d46c31b2ea5bdbb50e715e10dd2aaaf21e
willyb321 <wbwilliam7@gmail.com>
2018-01-09 06:20:35 +1100

    mod override for ?new

--- src/commands/reset.ts
+++ src/commands/reset.ts
@@ -10,6 +10,17 @@ import * as _ from 'lodash';
 import { collectors } from './index';
 
 export function reset(message: Discord.Message) {
+	if (message.member.roles.find(elem => config.allowedRoles.includes(elem.id))) {
+		currentStatus.currentUsers[message.channel.id] = [];
+		currentStatus.teams[message.channel.id] = [];
+		currentStatus.locked[message.channel.id] = false;
+		currentStatus.teamMessage[message.channel.id] = [];
+		currentStatus.teamsNumber[message.channel.id] = [];
+		collectors.forEach(elem => elem.cleanup());
+		collectors.slice(0, collectors.length);
+		message.reply('New session created.');
+		return;
+	}
 	if (!currentStatus.currentUsers[message.channel.id] || !currentStatus.currentUsers[message.channel.id].find(elem => elem === message.author)) {
 		return message.reply('You aren\'t in the session');
 	}
