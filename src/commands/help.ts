5499f65d13b8f444e3ceca639c7ab61a37f05a98
willyb321 <wbwilliam7@gmail.com>
2018-01-10 08:09:42 +1100

    add to help

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -13,7 +13,9 @@ export function help(message: Discord.Message) {
 		.addField('?unreg[ister]', 'Unregister yourself.')
 		.addField('?new', 'New session. Please use when finishing a match. (Mods can override)')
 		.addField('?restart', '[Mod] Restart the bot.')
-		.addField('?remove [@user]', '[Mod] Remove a user if they are AFK');
+		.addField('?remove [@user]', '[Mod] Remove a user if they are AFK')
+		.addField('?showgame [ID]', 'Show game info')
+		.addField('?result [ID] [Team # (0/1)]', '[Mod] Set the result of game #[ID]. Make sure to do this otherwise you\'ll end up with team #12 winning.')
 	return message.reply({embed});
 
 }
