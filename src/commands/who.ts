991e7d2a1041176dfa22f2557815b110ff370fff
willyb321 <wbwilliam7@gmail.com>
2018-01-09 16:22:33 +1100

    fix ?who

--- src/commands/who.ts
+++ src/commands/who.ts
@@ -9,13 +9,10 @@ import * as Discord from 'discord.js';
 
 export function who(message: Discord.Message) {
 	const usernames = [];
-	const embed = genEmbed(`Current Queue`, `${message.channel.toString()}`)
 	if (!currentStatus.currentUsers[message.channel.id]) {
 		currentStatus.currentUsers[message.channel.id] = [];
 	}
-	currentStatus.currentUsers[message.channel.id].forEach((elem: Discord.User, index: number) => {
-		embed.addField(index + 1, elem.toString());
-	});
+	const embed = genEmbed(`Current Queue:`, `${currentStatus.currentUsers[message.channel.id].join('\n')}`)
 	if (currentStatus.currentUsers[message.channel.id].length === 0) {
 		message.channel.send(`Nobody currently registered for ${message.channel.toString()}.`);
 		return;
