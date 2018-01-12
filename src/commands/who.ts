8cf38210ec87c414a9a0bc2dbcc7f60677713034
willyb321 <wbwilliam7@gmail.com>
2018-01-12 15:31:49 +1100

    fix ?who

--- src/commands/who.ts
+++ src/commands/who.ts
@@ -12,7 +12,7 @@ export function who(message: Discord.Message) {
 		currentStatus.currentUsers.set(message.channel.id, []);
 	}
 	const embed = genEmbed('Current Queue:', `${currentStatus.currentUsers.get(message.channel.id).join('\n')}`);
-	if (currentStatus.currentUsers.get(message.channel.id).length === 0) {
+	if (!currentStatus.currentUsers.has(message.channel.id) || currentStatus.currentUsers.get(message.channel.id).length === 0) {
 		return message.channel.send(`Nobody currently registered for ${message.channel.toString()}.`);
 	}
 	return message.channel.send({embed});
