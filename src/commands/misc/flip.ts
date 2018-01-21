40a25d10101738ef1968b0ee1e2b504b68edefe2
willyb321 <wbwilliam7@gmail.com>
2018-01-21 11:38:28 +1100

    add ?flip

---
+++ src/commands/misc/flip.ts
@@ -0,0 +1,55 @@
+/**
+ * @module Commands
+ */
+/**
+ * ignore
+ */
+import * as Discord from 'discord.js';
+import {config, genEmbed} from "../../utils";
+import * as Commando from 'discord.js-commando';
+import {basename} from "path";
+import * as Raven from "raven";
+
+Raven.config(config.ravenDSN, {
+	autoBreadcrumbs: true,
+	dataCallback: function (data) { // source maps
+		const stacktrace = data.exception && data.exception[0].stacktrace;
+
+		if (stacktrace && stacktrace.frames) {
+			stacktrace.frames.forEach(frame => {
+				if (frame.filename.startsWith('/')) {
+					frame.filename = 'app:///' + basename(frame.filename);
+				}
+			});
+		}
+
+		return data;
+	}
+}).install();
+
+/**
+ * Coin flip
+ */
+const flip = () => (Math.floor(Math.random() * 2) == 0) ? 'heads' : 'tails';
+
+
+export class StatusCommand extends Commando.Command {
+	constructor(client) {
+		super(client, {
+			name: 'flip',
+			group: 'misc',
+			memberName: 'flip',
+			description: 'Flip a theoretical coin.',
+			details: 'Flip a theoretical coin.',
+			examples: ['flip']
+		});
+	}
+
+	async run(msg, args) {
+		const flipped = flip();
+		console.log(`Coin flipped: ${flipped}`)
+		const embed = genEmbed('Coin Flipped', flipped);
+		return msg.channel.send({embed});
+	}
+
+}
