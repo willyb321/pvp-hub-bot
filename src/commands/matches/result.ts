d1ee6506c3030697478c346e5e8bf97074f19873
willyb321 <wbwilliam7@gmail.com>
2018-03-18 12:01:57 +1100

    oops

--- src/commands/matches/result.ts
+++ src/commands/matches/result.ts
@@ -59,7 +59,7 @@ export class ResultCommand extends Commando.Command {
 
 	hasPermission(message) {
 		if (!msg.member.roles.find(elem => config.allowedRoles.includes(elem.id))) {
-			reutrn false;
+			return false;
 		}
 		if (!isNaN(figureOutTeams(message))) {
 			return true;
